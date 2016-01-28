777cd6c8f829f90ca1922d898ba3ca0c4d0be394
Tomasz Woźniczka <tom.wozniczka@gmail.com>
2016-01-28 22:06:36 +0100

    Added Class PurchaseProof and one test

--- src/main/java/com/wozniczka/tomasz/paragony/PurchaseProof.java
+++ src/main/java/com/wozniczka/tomasz/paragony/PurchaseProof.java
@@ -1,4 +1,22 @@
 package com.wozniczka.tomasz.paragony;
 
 public class PurchaseProof {
+	private String productName;
+	private int productPrice;
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String name) {
+		productName = name;
+	}
+
+	public int getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(int productPrice) {
+		this.productPrice = productPrice;
+	}
 }
--- src/test/java/com/wozniczka/tomasz/paragony/PurchaseProofTest.java
+++ src/test/java/com/wozniczka/tomasz/paragony/PurchaseProofTest.java
@@ -1,5 +1,33 @@
 package com.wozniczka.tomasz.paragony;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PurchaseProofTest {
+
+	public static final String HEADPHONES = "Headphones";
+	public static final int PRICE = 400;
+	PurchaseProof purchaseProof;
+
+	@Before
+	public void setUp() {
+		purchaseProof = new PurchaseProof();
+	}
+
+	@Test
+	public void shouldBeAbleToAddProductName() {
+		purchaseProof.setProductName(HEADPHONES);
+
+		assertEquals(HEADPHONES, purchaseProof.getProductName());
+	}
+
+	@Test
+	public void shouldBeAbleToAddProductPrice() {
+		purchaseProof.setProductPrice(PRICE);
+
+		assertEquals(PRICE, purchaseProof.getProductPrice());
+	}
 
 }
