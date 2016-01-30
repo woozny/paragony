d5dff65117eb7082cd50cddc810556009641d6b6
Tomasz Woźniczka <tom.wozniczka@gmail.com>
2016-01-30 21:25:43 +0100

    Complete Invoice Class

--- src/main/java/com/wozniczka/tomasz/paragony/PurchaseProof.java
+++ src/main/java/com/wozniczka/tomasz/paragony/PurchaseProof.java
@@ -1,8 +1,15 @@
 package com.wozniczka.tomasz.paragony;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class PurchaseProof {
 	private String productName;
 	private int productPrice;
+	private int guaranteePeriod;
+	private BufferedImage invoiceImage;
 
 	public String getProductName() {
 		return productName;
@@ -18,5 +25,32 @@ public class PurchaseProof {
 
 	public void setProductPrice(int productPrice) {
 		this.productPrice = productPrice;
+	}
+
+	public int getGuaranteePeriod() {
+		return guaranteePeriod;
+	}
+
+	public void setGuaranteePeriod(int guaranteePeriod) {
+		this.guaranteePeriod = guaranteePeriod;
+	}
+
+	public BufferedImage getInvoiceImage() {
+		return invoiceImage;
+	}
+
+
+	public void addInvoiceImage(String invoiceImagePath) {
+		invoiceImage = loadInvoiceImage(invoiceImagePath);
+	}
+
+	private BufferedImage loadInvoiceImage(String imagePath) {
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File(imagePath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return img;
 	}
 }
--- src/test/java/com/wozniczka/tomasz/paragony/PurchaseProofTest.java
+++ src/test/java/com/wozniczka/tomasz/paragony/PurchaseProofTest.java
@@ -3,13 +3,21 @@ package com.wozniczka.tomasz.paragony;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PurchaseProofTest {
 
-	public static final String HEADPHONES = "Headphones";
-	public static final int PRICE = 400;
-	PurchaseProof purchaseProof;
+	private static final String HEADPHONES = "Headphones";
+	private static final String INVOICE_IMAGE_PATH = "/home/tomek/IdeaProjects/paragony/src/test/TestResources/427572.jpg";
+	private static final String INCORRECT_INVOICE_IMAGE_PATH = "test427572.jpg";
+	private static final int PRICE = 400;
+	private final BufferedImage sampleInvoiceImage = loadInvoiceImage(INVOICE_IMAGE_PATH);
+	private PurchaseProof purchaseProof;
 
 	@Before
 	public void setUp() {
@@ -20,14 +28,42 @@ public class PurchaseProofTest {
 	public void shouldBeAbleToAddProductName() {
 		purchaseProof.setProductName(HEADPHONES);
 
-		assertEquals(HEADPHONES, purchaseProof.getProductName());
+		assertThat(purchaseProof.getProductName()).isEqualTo(HEADPHONES);
 	}
 
 	@Test
 	public void shouldBeAbleToAddProductPrice() {
 		purchaseProof.setProductPrice(PRICE);
 
-		assertEquals(PRICE, purchaseProof.getProductPrice());
+		assertThat(purchaseProof.getProductPrice()).isEqualTo(PRICE);
+	}
+
+	@Test
+	public void shouldBeAbleToAddGuaranteePeriod() {
+		purchaseProof.setGuaranteePeriod(2);
+
+		assertThat(purchaseProof.getGuaranteePeriod()).isEqualTo(2);
+	}
+
+	@Test
+	public void shouldBeAbleToAddScannedInvoice() {
+		purchaseProof.addInvoiceImage(INVOICE_IMAGE_PATH);
+
+		assertThat(purchaseProof.getInvoiceImage().getHeight()).isEqualTo(sampleInvoiceImage.getHeight());
+		assertThat(purchaseProof.getInvoiceImage().getWidth()).isEqualTo(sampleInvoiceImage.getWidth());
+		assertThat(purchaseProof.getInvoiceImage().getType()).isEqualTo(sampleInvoiceImage.getType());
+	}
+
+//TODO: test for exception when image does not exists
+
+	private BufferedImage loadInvoiceImage(String imagePath) {
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File(imagePath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return img;
 	}
 
 }
