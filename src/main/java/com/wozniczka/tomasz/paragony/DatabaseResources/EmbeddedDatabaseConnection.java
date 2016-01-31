5a950e60dbe4c8c064aa3206a3d842f6849992ec
Tomasz Woźniczka <tom.wozniczka@gmail.com>
2016-01-31 16:43:31 +0100

    Small change in database configuration

--- src/main/java/com/wozniczka/tomasz/paragony/DatabaseResources/EmbeddedDatabaseConnection.java
+++ src/main/java/com/wozniczka/tomasz/paragony/DatabaseResources/EmbeddedDatabaseConnection.java
@@ -17,7 +17,12 @@ public class EmbeddedDatabaseConnection {
 
 
 	public EmbeddedDatabaseConnection() {
-		connection = connectToDatabase();
+		try {
+			connection = connectToDatabase();
+			statement = connection.createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void printSQLException(SQLException e) {
@@ -57,7 +62,6 @@ public class EmbeddedDatabaseConnection {
 
 	private void createTable() throws SQLException {
 		//TODO: Add column for images
-		statement = connection.createStatement();
 		statement.execute("create table " + TABLE_NAME + "(id int, " + PRODUCT_NAME_COLUMN + " varchar(100), " + PRODUCT_PRICE_COLUMN + " int, " + GUARANTEE_COLUMN + " int)");
 		System.out.println("Created table " + TABLE_NAME);
 
@@ -89,7 +93,7 @@ public class EmbeddedDatabaseConnection {
 		}
 	}
 
-	public Connection getConnection() {
-		return connection;
+	public Statement getStatement() {
+		return statement;
 	}
 }
